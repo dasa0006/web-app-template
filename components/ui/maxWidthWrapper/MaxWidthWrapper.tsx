371efc6a3997b1e5f5fd5215d77efa4c0b722e12
dasa0006 <dasa0006@stud.ek.dk>
2026-03-14 21:52:25 +0100

    feat: implemented maxwwrapper

---
+++ components/ui/maxWidthWrapper/MaxWidthWrapper.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+interface IMaxWidthWrapper {
+  className?: string;
+  children: ReactNode;
+}
+
+export const MaxWidthWrapper = ({ className, children }: IMaxWidthWrapper) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto w-full max-w-7xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};

96c7cd79e5c174220a01f475847e207721ec63bb
dasa0006 <dasa0006@stud.ek.dk>
2026-04-15 16:40:01 +0200

    fix: convert to about page to async & add related keys to both locale message files

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,17 +1,17 @@
 import { Section } from "@/components/sections/section/Section";
 import { Heading } from "@/components/ui/heading/Heading";
 import { MaxWidthWrapper } from "@/components/ui/maxWidthWrapper/MaxWidthWrapper";
+import { getTranslations } from "next-intl/server";
 
-const Page = () => {
+export default async function Page() {
+  const t = await getTranslations("About");
   return (
     <>
       <Section>
         <MaxWidthWrapper>
-          <Heading>About</Heading>
+          <Heading>{t("title")}</Heading>
         </MaxWidthWrapper>
       </Section>
     </>
   );
-};
-
-export default Page;
+}
